7e59680db76ddc594868d65fe4c3f6410967df55
tr1x <39650214+tr1xxx@users.noreply.github.com>
2021-04-22 00:38:27 +0100

    Update main.cpp

--- medusa-loader/medusa-loader/main.cpp
+++ medusa-loader/medusa-loader/main.cpp
@@ -5,9 +5,9 @@ int main()
 
 	VMProtectBeginUltra("main");
 
-	//std::thread antidbg_thread(app::antidbg_loop);
-	//std::thread random_thread(app::random_filename);
-	//app::checksum_loop();
+	std::thread antidbg_thread(app::antidbg_loop);
+	std::thread random_thread(app::random_filename);
+	app::checksum_loop();
 	std::thread login_thread(app::login_loop);
 	std::thread menu_render_thread(app::render_loop);
 
